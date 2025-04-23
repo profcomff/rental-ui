166c7716cb18f45314df8d556968c409d71a75fd
Dmatrushka19 <dmasik1903@gmail.com>
2025-04-23 13:50:57 +0300

    добавил тестировочные имя и описание для проверки вместимости длины

--- src/constants/mockData.ts
+++ src/constants/mockData.ts
@@ -68,6 +68,11 @@ export const mockItemTypes: ItemType[] = [
 		image_url: 'https://pingpong73.ru/media/images/articles/professionalnaya-raketka-butterfly.jpg',
 		description: 'для игры в пинг-понг',
 	},
+  {
+		id: 16,
+		name: 'Очень длинный-длинный-длинный-просто длиннющий текст',
+    description: 'Очень длинное-длинное-длинное-просто длиннющее описание',
+	},
 	{
 		id: 5,
 		name: 'проверка заглушки',
