db5429b4a8b1247925224bc6bcc5992e9d2a389c
Илья Батуев <batuev.io18@physics.msu.ru>
2025-12-01 20:04:44 +0300

    In progress of fixing sessions fetching in archive

--- src/pages/admin/AdminJournalV2.vue
+++ src/pages/admin/AdminJournalV2.vue
@@ -1,60 +1,47 @@
 <script setup lang="ts">
 import AdminSessionCard from '@/components/AdminSessionCard.vue';
 import AdminTabs from '@/components/AdminTabs.vue';
-import { RentalSession } from '@/models';
 import { useAdminStore } from '@/store';
 import { storeToRefs } from 'pinia';
-import { onBeforeMount, ref } from 'vue';
+import { computed, onBeforeMount, ref } from 'vue';
 
 onBeforeMount(async () => {
-	await requestFinishedPageSessions();
-	await requestCancelledPageSessions();
-
-	selectedSessions.value = finishedPageSessions.value;
+	requestFinishedPageSessions();
+	requestCancelledPageSessions();
 });
 
 const adminStore = useAdminStore();
 const { requestFinishedPageSessions, requestCancelledPageSessions } = adminStore;
 const { finishedPageSessions, cancelledPageSessions } = storeToRefs(adminStore);
 
-const selectedSessions = ref<RentalSession[]>([]);
+const selectedSessions = computed(() => {
+	if (tab.value === 'finished') return finishedPageSessions;
+	else return cancelledPageSessions;
+});
 
 const userId = ref<string>();
+
 async function handleSearchById() {
-	await requestFinishedPageSessions(userId.value === '' ? undefined : Number(userId.value));
-	await requestCancelledPageSessions(userId.value === '' ? undefined : Number(userId.value));
-	switchMode(tab.value);
+	requestFinishedPageSessions(userId.value === '' ? undefined : Number(userId.value));
+	requestCancelledPageSessions(userId.value === '' ? undefined : Number(userId.value));
 }
 
 const tab = ref<'finished' | 'cancelled'>('finished');
-function switchMode(tab: 'finished' | 'cancelled') {
-	if (tab === 'finished') {
-		selectedSessions.value = finishedPageSessions.value;
-	} else if (tab === 'cancelled') {
-		selectedSessions.value = cancelledPageSessions.value;
-	}
-}
 </script>
 
 <template>
 	<AdminTabs current-tab="/admin/journal" />
 	<v-text-field
 		v-model="userId"
-		class="my-2 mx-3"
+		class="my-2 py-2 mx-2"
 		placeholder="Поиск студента (по id)"
 		:prepend-inner-icon="'mdi-magnify'"
 		variant="outlined"
 		@update:model-value="handleSearchById"
 	></v-text-field>
-	<v-tabs
-		class="my-2"
-		align-tabs="center"
-		v-model="tab"
-		selected-class="bg-primary"
-		@update:model-value="switchMode(tab)"
-	>
+	<v-tabs class="my-2" align-tabs="center" v-model="tab" selected-class="bg-primary">
 		<v-tab variant="elevated" value="finished">Завершенные</v-tab>
 		<v-tab variant="elevated" value="cancelled">Отмененные</v-tab>
 	</v-tabs>
-	<AdminSessionCard v-for="s in selectedSessions" :key="s.id" :session="s" location="journal" />
+	<AdminSessionCard v-for="s in selectedSessions" :key="s" :session="s" location="journal" />
 </template>
